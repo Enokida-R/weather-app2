7b3e6fa6f2b4377840ba52cf72995397c1ab1bbb
Enokida-R <raita.5006@gmail.com>
2023-12-19 04:11:28 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const request = require("request");
+const dotenv = require("dotenv").config();
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/weather', (req,res) => {
+    const location = req.query.location;
+
+    if (!location) {
+        return res.status(400).send('場所が指定されていません');
+    }
+
+    const options = {
+        url: `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${process.env.API_KEY}`,
+        method: "GET",
+        json: true,
+    };
+
+    request(options,(error, response, body) => {
+        if (error || response.statusCode !== 200) {
+            return res.status(500).send('APIリクエストに失敗しました');
+        }
+
+        res.json(body);
+    });
+})
+
+app.listen(port, () =>{
+    console.log(`Server running at http://localhost:${port}`);
+});
---
+++ public/script.js
@@ -0,0 +1,85 @@
+document.getElementById('weatherForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const location = document.getElementById('locationInput').value;
+
+    fetch(`/weather?location=${encodeURIComponent(location)}`)
+    .then(response => response.json())
+    .then(data => {
+        if (data) {
+            const bodyElement = document.querySelector('body');
+            const weather = data.weather[0];
+
+
+            let weatherImage = '';
+            switch (weather.main) {
+                case 'Clear':
+                    backgroundImageUrl = 'url(./weather/sunny.png)';
+                    break;
+                case 'Clouds':
+                    backgroundImageUrl = 'url(./weather/clouds.png)';
+                    break;
+                case 'Rain':
+                    backgroundImageUrl = 'url(./weather/Rain.png)';
+                    break;    
+                case 'Snow':
+                    backgroundImageUrl = 'url(./weather/snow.png)';
+                break;
+                case 'Thubderstom':
+                    backgroundImageUrl = 'url(./weather/thunderstom.png)';
+                break;
+                case 'Drizzle':
+                    backgroundImageUrl = 'url(./weather/drizzle.png)';
+                break;
+                case 'MIst':
+                    backgroundImageUrl = 'url(./weather/mist.png)';
+                break;
+                case 'Smoke':
+                    backgroundImageUrl = 'url(./weather/smoke.png)';
+                break;
+                case 'Haze':
+                    backgroundImageUrl = 'url(./weather/haze.png)';
+                break;
+                case 'Dust':
+                    backgroundImageUrl = 'url(./weather/dust.png)';
+                break;
+                case 'Fog':
+                    backgroundImageUrl = 'url(./weather/fog.png)';
+                break;
+                case 'Sand':
+                    backgroundImageUrl = 'url(./weather/sand.png)';
+                break;
+                case 'Ash':
+                    backgroundImageUrl = 'url(./weather/ash.png)';
+                break;
+                case 'Squall':
+                    backgroundImageUrl = 'url(./weather/squall.png)';
+                break;
+                case 'Tirnado':
+                    backgroundImageUrl = 'url(./weather/tornado.png)';
+                break;
+            }
+
+            // CSS を使って背景画像を設定
+            bodyElement.style.backgroundImage = backgroundImageUrl;
+            bodyElement.style.backgroundSize = 'cover'; // 背景画像をカバー全体に
+            bodyElement.style.backgroundRepeat = 'no-repeat'; // 繰り返し無し
+            bodyElement.style.backgroundPosition = 'center center'; // 中央に配置
+
+
+            document.getElementById('weatherResult').innerHTML =
+                `${weather.main}<br>
+            現在の気温:${data.main.temp}°<br>
+            最低気温:${data.main.temp_min}°<br>
+            最高気温:${data.main.temp_max}°<br>
+            湿度:${data.main.humidity}%`
+        } else {
+            document.getElementById('weatherResult').innerHTML = '情報を取得できませんでした。';
+        }
+        document.getElementById('weatherResult').style.color = 'blue'; // 文字色を青に設定
+
+    })
+    .catch(error => {
+        console.error('エラー', error);
+        document.getElementById('weatherResult').innerHTML = 'エラーが発生しました。'
+    });
+});
